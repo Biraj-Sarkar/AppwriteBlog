d9db1a00775e8d0bae926df21a8036f6d54182cb
Biraj <cs242@iitg.ac.in>
2025-12-16 17:15:22 +0530

    Router set

--- src/main.jsx
+++ src/main.jsx
@@ -71,7 +71,7 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <RouterProvider router={router} basename="/AppwriteBlog" />
     </Provider>
   </ StrictMode>,
 )
